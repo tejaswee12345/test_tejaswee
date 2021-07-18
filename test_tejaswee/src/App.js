c8b06b5249f94770fa79cc06d30b294c7462c168
tejaswee12345 <35096581+tejaswee12345@users.noreply.github.com>
2021-07-18 21:24:48 +0530

    Add files via upload

---
+++ test_tejaswee/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Student from './Student/Students';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Student></Student>
+      </header>
+    </div>
+  );
+}
+
+export default App;
